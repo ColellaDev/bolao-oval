adc2a21f1abc3c3833035cd280a593a140cc9296
Marcos Colella <colella.dev@gmail.com>
2025-07-07 11:22:20 -0300

    Implementado função loading no Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,20 @@
 import { useAuth } from '@/hooks/useAuth'
 
 export function Header() {
-  const { user, logout } = useAuth()
+  const { user, logout, loading } = useAuth()
 
   return (
     <header className="w-full bg-surface text-text px-6 py-4 flex justify-between items-center shadow-md">
       <h1 className="text-lg font-semibold text-primary">Bolão Oval</h1>
 
-      {user && (
+      {loading && (
+        <div className="flex items-center gap-4 animate-pulse">
+          <div className="h-5 w-32 rounded-md bg-zinc-700" />
+          <div className="h-8 w-16 rounded-lg bg-zinc-700" />
+        </div>
+      )}
+
+      {!loading && user && (
         <div className="flex items-center gap-4">
           <span className="text-sm text-muted">Olá, <strong>{user.name}</strong></span>
           <button
